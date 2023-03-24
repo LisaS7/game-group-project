534e2193942190d594959baea47e4b767b97faee
Lisa <mclisa00@gmail.com>
2023-03-24 11:28:05 +0000

    Fetch request working. Added filters for category and difficulty.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import GameContainer from "./containers/GameContainer";
 
 function App() {
   return (
     <div className="App">
       <h1>Template App</h1>
+      <GameContainer />
     </div>
   );
 }
