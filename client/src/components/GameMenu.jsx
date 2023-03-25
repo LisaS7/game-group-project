246f32a45f762a4ec87bb5bda2811e4fba2f4284
Lisa <mclisa00@gmail.com>
2023-03-25 19:20:51 +0000

    Capitalised button text

--- client/src/components/GameMenu.jsx
+++ client/src/components/GameMenu.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { categories, difficulties } from "../constants";
+import { Capitalise } from "../utils/capitalise";
 let selectedCategories = [];
 
-export function GameMenu({ setStartGame, setCategory, setDifficulty}) {
-
-  const [hiddenCategory, setHiddenCategory] = useState(true)
-  const [hiddenDifficulty, setHiddenDifficulty] = useState(true)
+export function GameMenu({ setStartGame, setCategory, setDifficulty }) {
+  const [hiddenCategory, setHiddenCategory] = useState(true);
+  const [hiddenDifficulty, setHiddenDifficulty] = useState(true);
 
   function handleClick(e) {
     selectedCategories.push(e.target.textContent);
@@ -14,51 +14,43 @@ export function GameMenu({ setStartGame, setCategory, setDifficulty}) {
 
   const categoryElements = categories.map((category, index) => (
     <button key={index} onClick={(e) => handleClick(e)}>
-      {category.replaceAll('_', ' ')}
+      {Capitalise(category.replaceAll("_", " "))}
     </button>
   ));
 
-
   const difficultyElements = difficulties.map((difficulty, index) => (
     <button key={index} onClick={(e) => setDifficulty(e.target.textContent)}>
-      {difficulty}
+      {Capitalise(difficulty)}
     </button>
   ));
 
   const handleHiddenCategory = () => {
     setHiddenCategory(!hiddenCategory);
     setHiddenDifficulty(true);
-  }
+  };
 
   const handleHiddenDifficulty = () => {
     setHiddenDifficulty(!hiddenDifficulty);
     setHiddenCategory(true);
-  }
+  };
 
   const handleStartGame = () => {
     setStartGame(true);
-  }
+  };
 
   return (
     <div>
-
       <button onClick={handleStartGame}>Start Game!</button>
 
       <div>
         <button onClick={handleHiddenCategory}>Select Category</button>
-        <div hidden={hiddenCategory}>
-          {categoryElements}
-        </div>
+        <div hidden={hiddenCategory}>{categoryElements}</div>
       </div>
 
       <div>
         <button onClick={handleHiddenDifficulty}>Select Difficulty</button>
-        <div hidden={hiddenDifficulty}>
-          {difficultyElements}
-        </div>
+        <div hidden={hiddenDifficulty}>{difficultyElements}</div>
       </div>
-      
     </div>
   );
 }
-
