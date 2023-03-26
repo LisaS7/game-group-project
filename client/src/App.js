a9cbc52be8215dfb3b629f4d7b5c05f7e23360ee
Shazreen Seatter <shazseatter12@gmail.com>
2023-03-26 13:47:19 +0100

    moved location of game title so that it doesn't appear again in the quiz container

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import GameContainer from "./containers/GameContainer";
 function App() {
   return (
     <div className="App">
-     <div class="logo"><b>M<span>in</span>d<span></span> <span>B</span>lan<span>k</span></b></div>
       <GameContainer />
     </div>
   );
