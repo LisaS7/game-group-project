51c0ecd0c16f8a0259c02dba8bb8d8cf6ae1e2b2
Lisa <mclisa00@gmail.com>
2023-03-26 09:22:09 +0100

    Score has an animated + appear when increased

--- client/src/containers/QuizContainer.jsx
+++ client/src/containers/QuizContainer.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Answer from "../components/Answer";
-// import Highscore from "../components/Highscore";
 import Question from "../components/Question";
 import { getHighscores } from "../HighscoreService";
 import { answerDelay } from "../constants";
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
-
-
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { motion } from "framer-motion";
+import "./QuizContainer.css";
 
 export default function QuizContainer({ data }) {
   const [questions, setQuestions] = useState([]);
   const [displayAnswer, setDisplayAnswer] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
-  const [highscores, setHighscores] = useState([])
-  const [score, setScore] = useState(0)
+  const [highscores, setHighscores] = useState([]);
+  const [score, setScore] = useState(0);
 
   function questionAnswered() {
     setDisplayAnswer(true);
@@ -23,14 +22,12 @@ export default function QuizContainer({ data }) {
     }, answerDelay);
   }
 
-
   function correctAnswer() {
-   setIsCorrect(true);
-   // add points to score
-   setScore(score + 1)
-   // post new score to db (waiting on function for game ending)
+    setIsCorrect(true);
+    // add points to score
+    setScore(score + 1);
+    // post new score to db (waiting on function for game ending)
   }
-
 
   useEffect(() => {
     setQuestions(data);
@@ -41,14 +38,16 @@ export default function QuizContainer({ data }) {
     setIsCorrect(false);
   }, [questions]);
 
+  useEffect(() => {
+    getHighscores().then((allHighscores) => {
+      setHighscores(allHighscores);
+    });
+  }, []);
 
-  useEffect(()=>{
-    getHighscores().then((allHighscores)=>{
-    setHighscores(allHighscores);
-    })
-}, []);
-
-const highestScore = Math.max.apply(Math, highscores.map(score => score.highscore))
+  const highestScore = Math.max.apply(
+    Math,
+    highscores.map((score) => score.highscore)
+  );
 
   if (!questions.length) return "Loading...";
 
@@ -56,44 +55,75 @@ const highestScore = Math.max.apply(Math, highscores.map(score => score.highscor
   incorrectAnswers.push(questions[0].correctAnswer);
   const allAnswers = [...new Set(incorrectAnswers)].sort();
 
- 
+  const variants = {
+    initial: { opacity: 1 },
+    correct: {
+      opacity: [0, 1, 0],
+      transition: { duration: 1.5, delay: 0.5 },
+    },
+    incorrect: {
+      opacity: 0,
+    },
+  };
 
   return (
     <>
-    <div>
-      <p>Highscore {highestScore}</p>
-      <p>Score {score}</p>
-    </div>
+      <div>
+        <p>Highscore {highestScore}</p>
+        <p>
+          Score <span>{score}</span>
+          <motion.span
+            className="score-plus"
+            initial="initial"
+            animate={isCorrect ? "correct" : "incorrect"}
+            variants={variants}
+          >
+            &nbsp;+
+          </motion.span>
+        </p>
+      </div>
 
-    <div>
-      {displayAnswer ? (
-        <p>{isCorrect ?  <Player
-          autoplay
-          speed="1"
-          src="https://assets8.lottiefiles.com/packages/lf20_xj3qhpxz.json"
-          style={{ height: '200px', width: '200px' }}
-      >
-              <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-          </Player>: <Player
-          autoplay
-          speed="1"
-          src="https://assets8.lottiefiles.com/packages/lf20_2bjwh0kp.json"
-          style={{ height: '200px', width: '200px' }}
-      >
-              <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-          </Player>}</p>
-      ) : (
-        <Question question={questions[0].question} />
-      )}
+      <div>
+        {displayAnswer ? (
+          <p>
+            {isCorrect ? (
+              <Player
+                autoplay
+                speed="1"
+                src="https://assets8.lottiefiles.com/packages/lf20_xj3qhpxz.json"
+                style={{ height: "200px", width: "200px" }}
+              >
+                <Controls
+                  visible={false}
+                  buttons={["play", "repeat", "frame", "debug"]}
+                />
+              </Player>
+            ) : (
+              <Player
+                autoplay
+                speed="1"
+                src="https://assets8.lottiefiles.com/packages/lf20_2bjwh0kp.json"
+                style={{ height: "200px", width: "200px" }}
+              >
+                <Controls
+                  visible={false}
+                  buttons={["play", "repeat", "frame", "debug"]}
+                />
+              </Player>
+            )}
+          </p>
+        ) : (
+          <Question question={questions[0].question} />
+        )}
 
-      <Answer
-        correct={questions[0].correctAnswer}
-        allAnswers={allAnswers}
-        questionAnswered={questionAnswered}
-        correctAnswer={correctAnswer}
-        isCorrect={isCorrect}
-      />
-    </div>
+        <Answer
+          correct={questions[0].correctAnswer}
+          allAnswers={allAnswers}
+          questionAnswered={questionAnswered}
+          correctAnswer={correctAnswer}
+          isCorrect={isCorrect}
+        />
+      </div>
     </>
   );
 }
