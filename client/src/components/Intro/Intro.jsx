9f2c5e8d84ee6bd7d865796ae0c8ffcaf5d605ea
nicolasgoetschel <nicgoe@hotmail.com>
2023-03-29 13:01:08 +0100

    intro sound working

--- client/src/components/Intro/Intro.jsx
+++ client/src/components/Intro/Intro.jsx
@@ -1,8 +1,10 @@
+import { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./intro.css";
 import hostImg from "./host.png";
 import speechBubble from "./speech_bubble.png";
 import starButton from "./star-button.png";
+import backgroundMusic from "../Music/intro.mp3";
 
 const hostVariants = {
   initial: { x: "50vw" },
@@ -19,7 +21,17 @@ const buttonVariants = {
   animate: { opacity: 1, rotate: 0, transition: { duration: 1, delay: 4 } },
 };
 
-export default function Intro({ setIntro }) {
+export default function Intro({ setIntro, handleClick, isPlaying }) {
+
+
+  const song = useRef(new Audio(backgroundMusic));
+
+  useEffect(() => {
+  
+    isPlaying ? song.current.pause() : song.current.play();
+  },[isPlaying, []])
+  
+
   return (
     <motion.div className="intro-container" initial="initial" animate="animate">
       <div className="host-container">
@@ -27,7 +39,7 @@ export default function Intro({ setIntro }) {
         <motion.div
           className="speech-bubble-container"
           variants={speechVariants}
-        >
+          >
           <motion.img className="speech-bubble" src={speechBubble} />
         </motion.div>
       </div>
@@ -36,7 +48,7 @@ export default function Intro({ setIntro }) {
         className="intro-button"
         variants={buttonVariants}
         src={starButton}
-        onClick={() => setIntro(false)}
+        onClick={handleClick}
       />
     </motion.div>
   );
